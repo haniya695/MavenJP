6fad733a16af0cb0d30a4492f6053d47e6818fdc
haniya <fatimahaniya016@gmail.com>
2024-11-16 15:31:46 +0530

    First commit to maven java project

---
+++ src/main/java/MavenJP/javaProject/App.java
@@ -0,0 +1,15 @@
+package MavenJP.javaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Haniya Fatima" );
+        System.out.println( "22BD1A664R" );
+    }
+}
